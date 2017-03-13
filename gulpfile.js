015a4ea0555d8c834f1326b644855f7a354e106c
Alessandro Verdura <alessandro@compucorp.co.uk>
2017-03-13 19:21:04 +0100

    PCHR-2064: Changes made in org.civicrm.bootstrapcivicrm

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,7 @@ var postcss = require('gulp-postcss');
 var postcssPrefix = require('postcss-prefix-selector');
 var postcssDiscardDuplicates = require('postcss-discard-duplicates');
 var stripCssComments = require('gulp-strip-css-comments');
-var transformSelectors = require("gulp-transform-selectors");
 var civicrmScssRoot = require('civicrm-scssroot')();
-
-var bootstrapNamespace = '#bootstrap-theme';
 
 gulp.task('sass-bootstrap', ['sass-sync'], function () {
   gulp.src('scss/bootstrap/bootstrap.scss')
@@ -19,11 +16,6 @@ gulp.task('sass-bootstrap', ['sass-sync'], function () {
       precision: 10
     }).on('error', sass.logError))
     .pipe(stripCssComments({ preserve: false }))
-    .pipe(postcss([postcssPrefix({
-      prefix: bootstrapNamespace + ' ',
-      exclude: [/^html/, /^body/]
-    })]))
-    .pipe(transformSelectors(namespaceRootElements, { splitOnCommas: true }))
     .pipe(gulp.dest('css/'));
 });
 
@@ -54,21 +46,3 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['sass', 'watch']);
-
-/**
- * Apply the namespace on html and body elements
- *
- * @param  {string} selector the current selector to be transformed
- * @return string
- */
-function namespaceRootElements(selector) {
-  var regex = /^(body|html)/;
-
-  if (regex.test(selector)) {
-    selector = selector.replace(regex, function (match) {
-      return match + bootstrapNamespace;
-    }) + ",\n" + selector.replace(regex, bootstrapNamespace);
-  }
-
-  return selector;
-}
